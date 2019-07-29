7a88f3e700b3e8a055f197c822f893ddac65fbab
Katherine Lewis <kale8432@colorado.edu>
2019-07-29 09:54:06 -0600

    building out app, will fetch data, but will not turn into props

--- src/apicall.js
+++ src/apicall.js
@@ -2,7 +2,7 @@ export const getArtists = async () => {
   const url = "http://localhost:3002/api/v1/artists";
   try{
     const response = await fetch(url);
-    return await response.json();
+   return await response.json();
   } catch(error){
     throw new Error(error.message);
   }
